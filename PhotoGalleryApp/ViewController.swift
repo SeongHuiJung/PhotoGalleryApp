540bced89026793bfc8142eef8aeefb351be641b
Jseong <tjdgml150237@naver.com>
2024-02-22 19:06:00 +0900

    Initial Commit

---
+++ PhotoGalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by 정성희 on 2024/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
